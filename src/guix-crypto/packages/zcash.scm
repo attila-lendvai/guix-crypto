be4e3a44356e4928582ce3535ec8629924e040ce
Attila Lendvai <attila@lendvai.name>
2022-04-29 15:49:09 +0200

    WIP add zecwallet-lite-binary

--- src/guix-crypto/packages/zcash.scm
+++ src/guix-crypto/packages/zcash.scm
@@ -33,7 +33,11 @@
   #:use-module (gnu packages compression)
   #:use-module (gnu packages elf)
   #:use-module (gnu packages gcc)
+  #:use-module (gnu packages glib)
   #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages nss)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages xorg)
   #:use-module (nonguix build-system binary)
   #:use-module (ice-9 match))
 
@@ -112,4 +116,99 @@
       (home-page "https://z.cash")
       (synopsis "Zcash blockchain tools")
       (description "The official Zcash binary release, patched to work on Guix.")
+      (license license:expat))))
+
+;;;
+;;; Zecwallet Lite
+;;;
+
+;; https://raw.githubusercontent.com/ZcashFoundation/zecwallet/master/public_key.asc
+;; captured at 2022-04-29.
+(define +zecwallet-key-fingerprint+
+  "C23172D0C9569591ECEC8ECB0E1E90279521EBB4")
+
+(define* (zecwallet-lite-binary-origin version hash #:optional (prefix "Zecwallet_Lite_")
+                                       (suffix "_amd64.deb"))
+  (let ((file-name (string-append prefix version suffix)))
+    (origin
+      (method url-fetch)
+      (uri (simple-format #f "https://github.com/adityapk00/zecwallet-lite/releases/download/v~A/~A"
+                          version file-name))
+      (file-name file-name)
+      (sha256 hash))))
+
+(define-public zecwallet-lite-binary
+  (let* ((version "1.7.8")
+         (signing-key (local-file "../../../keys/zecwallet-signing-key.asc"))
+         (signatures
+          (zecwallet-lite-binary-origin
+           version
+           (match (%current-system)
+             ("x86_64-linux"
+              (base32 "18h48jlnjqjb432hsfcjf6n6c8hfvyd4cmcg8i8m2ivhxgjqnfkr"))
+             (_ (unsupported-arch name (%current-system))))
+           "signatures-"
+           ".zip")))
+    (package
+      (name "zecwallet-lite-binary")
+      (version version)
+      (source (zecwallet-lite-binary-origin
+               version
+               (match (%current-system)
+                 ("x86_64-linux"
+                  (base32 "1c1rqamy5m4vg47d2s85qai50klly7acdz1dmk3wbwasx473c5z0"))
+                 (_ (unsupported-arch name (%current-system))))))
+      (build-system binary-build-system)
+      (arguments
+       (list
+        #:imported-modules (source-module-closure
+                            `((guix-crypto build-utils)
+                              ,@%binary-build-system-modules)
+                            #:select? default-module-filter)
+        #:modules '((guix build utils)
+                    (guix-crypto build-utils)
+                    (nonguix build binary-build-system))
+        #:install-plan ''(("opt/Zecwallet Lite/zecwallet-lite" "bin/")
+                          ("opt/Zecwallet Lite/libffmpeg.so"   "lib/")
+                          ("usr/share/" "share/"))
+        #:strip-binaries? #false        ; The less we modify, the better.
+        #:patchelf-plan ''(("opt/Zecwallet Lite/libffmpeg.so"
+                            ("glibc" "gcc"))
+                           ("opt/Zecwallet Lite/zecwallet-lite"
+                            ( ;;"ffmpeg"
+                             "glib" "glibc" "gcc" "libxshmfence" ("nss" "/lib/nss"))))
+        #:phases
+        #~(modify-phases %standard-phases
+            (add-before 'unpack 'check-signatures
+              (lambda* (#:key source #:allow-other-keys)
+                (invoke "unzip" #$signatures)
+                (let ((signature #$(simple-format #f "signatures/Zecwallet_Lite_~A_amd64.deb.sig"
+                                                  version)))
+                  (verify-gpg-signature #$+zecwallet-key-fingerprint+
+                                        #$signing-key
+                                        signature
+                                        source))))
+            (replace 'unpack
+              (lambda* (#:key source #:allow-other-keys)
+                (invoke "ar" "x" source)
+                (invoke "tar" "-xJvf" "data.tar.xz"))))))
+      (native-inputs
+       (list binutils ; for ar to extrat .deb files
+             gnupg
+             patchelf
+             unzip))
+      (inputs
+       (list ;;ffmpeg-2.8
+             (list gcc "lib")
+             glib
+             glibc
+             libxshmfence
+             nss
+             ))
+      (supported-systems '("x86_64-linux"))
+      (home-page "https://www.zecwallet.co/")
+      (synopsis "Fully featured shielded wallet for Zcash")
+      (description "Zecwallet Lite is a fully featured shielded wallet for Zcash.
+
+Syncs in under a minute. No blockchain download needed!")
       (license license:expat))))
