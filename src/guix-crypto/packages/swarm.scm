8e3df6f1547f8cfc70896cff45295c0a39bdd3e1
Attila Lendvai <attila@lendvai.name>
2022-05-16 22:22:38 +0200

    bee-binary: update to 1.6.0

--- src/guix-crypto/packages/swarm.scm
+++ src/guix-crypto/packages/swarm.scm
@@ -35,7 +35,8 @@
   #:use-module (srfi srfi-1))
 
 (define-public bee-binary
-  (let ((version "1.5.1")
+  (let ((version "1.6.0")
+        ;; Note: use bin/geth-update-helper.scm to update the hashes
         (hashes (read-module-relative-file "bee-binary.hashes")))
     (package
       (name "bee-binary")
