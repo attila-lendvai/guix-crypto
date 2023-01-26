84835897de115ef86ccc2b2d6a105848d412e60a
Attila Lendvai <attila@lendvai.name>
2023-01-26 11:10:23 +0100

    nethermind-binary: update to 1.16.1

--- src/guix-crypto/packages/ethereum.scm
+++ src/guix-crypto/packages/ethereum.scm
@@ -157,8 +157,8 @@ programming language.")
        '((release-monitoring-url . "https://github.com/openethereum/openethereum/releases"))))))
 
 (define-public nethermind-binary
-  (let* ((version "1.16.0")
-         (commit "c7df68c6")
+  (let* ((version "1.16.1")
+         (commit "644fe89f")
          ;; Note: use bin/geth-update-helper.scm to update the hashes
          (hashes (read-module-relative-file "nethermind-binary.hashes")))
     (package
