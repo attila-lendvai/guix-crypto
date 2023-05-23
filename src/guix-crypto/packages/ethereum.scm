419c5a77121d0489250b96dd30ac6031794d6f9a
Attila Lendvai <attila@lendvai.name>
2023-05-23 07:49:39 +0200

    nethermind-binary: update to 1.18.1

--- src/guix-crypto/packages/ethereum.scm
+++ src/guix-crypto/packages/ethereum.scm
@@ -102,7 +102,7 @@ censorship, fraud or third party interference.")
        '((release-monitoring-url . "https://github.com/ethereum/go-ethereum/releases"))))))
 
 (define-public nethermind-binary
-  (let ((commit-hash "97e80dda")       ; first 8 digits of the tagged commit's hash
+  (let ((commit-hash "ea6ca77f")       ; first 8 digits of the tagged commit's hash
         (version hashes (read-hashes-file "nethermind-binary")))
     (package
       (name "nethermind-binary")
