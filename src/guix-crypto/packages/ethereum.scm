dd244440407f16f4675b6706dbd8193ec9998a24
Attila Lendvai <attila@lendvai.name>
2023-12-04 16:30:36 +0100

    nethermind-binary: update to 1.23.0

--- src/guix-crypto/packages/ethereum.scm
+++ src/guix-crypto/packages/ethereum.scm
@@ -127,7 +127,7 @@ censorship, fraud or third party interference.")
                     (("build_tools/gnu_parallel") "parallel"))))))))
 
 (define-public nethermind-binary
-  (let ((commit-hash "ae444a4b")       ; first 8 digits of the tagged commit's hash
+  (let ((commit-hash "bcd3b8ae")       ; first 8 digits of the tagged commit's hash
         (version hashes (read-hashes-file "nethermind-binary")))
     (package
       (name "nethermind-binary")
